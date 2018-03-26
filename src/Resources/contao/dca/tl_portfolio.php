6e9a0aea22985dc105b98e6780b2a834f8b63afe
Oliver Lohoff <info@contao4you.de>
2018-03-26 18:28:50 +0200

    Unify imgSize/size attribute and use the correct imgSize

--- src/Resources/contao/dca/tl_portfolio.php
+++ src/Resources/contao/dca/tl_portfolio.php
@@ -125,7 +125,7 @@ $GLOBALS['TL_DCA']['tl_portfolio'] = array
     // Subpalettes
     'subpalettes' => array
     (
-		'addImage'                    => 'singleSRC,size,floating,imagemargin,fullsize,overwriteMeta',
+		'addImage'                    => 'singleSRC,imgSize,floating,imagemargin,fullsize,overwriteMeta',
         'source_internal'             => 'jumpTo',
         'source_article'              => 'articleId',
         'source_external'             => 'url,target',
@@ -241,7 +241,7 @@ $GLOBALS['TL_DCA']['tl_portfolio'] = array
 			'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
 			'sql'                     => "varchar(255) NOT NULL default ''"
 		),
-		'size' => array
+		'imgSize' => array
 		(
 			'label'                   => &$GLOBALS['TL_LANG']['tl_content']['size'],
 			'exclude'                 => true,
