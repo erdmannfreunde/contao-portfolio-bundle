a3435d08a14bc69fe643dfc7c5164fb84d83bdd9
Dennis Erdmann <dennis@erdmann-freunde.de>
2018-03-27 09:51:39 +0200

    remove the distriction to specific content elements

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -6,36 +6,4 @@
 if (Input::get('do') == 'portfolio')
 {
     $GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'tl_portfolio';
-    $GLOBALS['TL_DCA']['tl_content']['fields']['type']['options_callback'] = array('tl_content_portfolio', 'getContentElements');
-}
-
-/**
- * Class tl_content_portfolio
- *
- * Provide miscellaneous methods that are used by the data configuration array.
- */
-class tl_content_portfolio extends tl_content
-{
-    /**
-     * Return all portfolio content elements as array
-     * @return array
-     */
-    public function getContentElements()
-    {
-        $arrGroups = array();
-        $arrAllowedElements = array('headline', 'text', 'image', 'gallery', 'dma_eg_3', 'dma_eg_6', 'dma_eg_5');
-
-        foreach (parent::getContentElements() as $strGroup => $arrElements)
-        {
-            foreach (array_values($arrElements) as $strElement)
-            {
-                if (in_array($strElement, $arrAllowedElements))
-                {
-                    $arrGroups[$strGroup][] = $strElement;
-                }
-            }
-        }
-
-        return $arrGroups;
-    }
 }
