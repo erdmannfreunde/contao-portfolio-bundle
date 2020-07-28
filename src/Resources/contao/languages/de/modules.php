ed1bc38acdb264ab803fd6a1bd320a72039cb5a1
Dennis Erdmann <dennis@erdmann-freunde.de>
2020-07-28 15:57:45 +0200

    Title Referenzen --> Portfolio

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -3,13 +3,13 @@
 /**
  * backend modules
  */
-$GLOBALS['TL_LANG']['MOD']['portfolio'] = array("Referenzen", 'Hier können Sie Ihre Referenzen verwalten.');
+$GLOBALS['TL_LANG']['MOD']['portfolio'] = array("Portfolio", 'Hier können Sie Ihr Portfolio verwalten.');
 
 
 /**
  * frontend modules
  */
-$GLOBALS['TL_LANG']['FMD']['portfolio'] = "Referenzen";
+$GLOBALS['TL_LANG']['FMD']['portfolio'] = "Portfolio";
 
-$GLOBALS['TL_LANG']['FMD']['portfoliolist']     = array('Referenzenliste', '');
-$GLOBALS['TL_LANG']['FMD']['portfolioreader']   = array('Referenzenleser', '');
+$GLOBALS['TL_LANG']['FMD']['portfoliolist']     = array('Portfolioliste', '');
+$GLOBALS['TL_LANG']['FMD']['portfolioreader']   = array('Portfolioleser', '');
--- src/Resources/contao/languages/de/tl_portfolio_category.php
+++ src/Resources/contao/languages/de/tl_portfolio_category.php
@@ -1,18 +1,6 @@
 <?php
 
 /**
- * news_categories extension for Contao Open Source CMS
- *
- * Copyright (C) 2011-2014 Codefog
- *
- * @package news_categories
- * @author  Webcontext <http://webcontext.com>
- * @author  Codefog <info@codefog.pl>
- * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
- * @license LGPL
- */
-
-/**
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_portfolio_category']['title']             = array('Titel', 'Bitte Kategorie-Titel eingeben.');
