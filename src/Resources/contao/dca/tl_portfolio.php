bea9e11f0b9e5f2fe0cdb6e970a3effd5435a27a
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2022-07-20 16:19:12 +0200

    Fix undefined array keys for deleteConfirm

--- src/Resources/contao/dca/tl_portfolio.php
+++ src/Resources/contao/dca/tl_portfolio.php
@@ -86,7 +86,7 @@ $GLOBALS['TL_DCA']['tl_portfolio'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_portfolio']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'toggle'     => [
                 'label'           => &$GLOBALS['TL_LANG']['tl_portfolio']['toggle'],
@@ -179,7 +179,7 @@ $GLOBALS['TL_DCA']['tl_portfolio'] = [
             'sql'       => "varchar(255) NOT NULL default ''",
         ],
         'pageTitle' => [
-        
+
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'text',
--- src/Resources/contao/dca/tl_portfolio_category.php
+++ src/Resources/contao/dca/tl_portfolio_category.php
@@ -69,7 +69,7 @@ $GLOBALS['TL_DCA']['tl_portfolio_category'] = [
                 'label'               => &$GLOBALS['TL_LANG']['tl_portfolio_category']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes'          => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show' => [
                 'label'               => &$GLOBALS['TL_LANG']['tl_portfolio_category']['show'],
