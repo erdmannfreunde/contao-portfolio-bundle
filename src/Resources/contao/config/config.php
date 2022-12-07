55d9f598d22c333c6ac351dccb52aa10260447df
Dennis Erdmann <dennis@erdmann-freunde.de>
2022-12-07 14:40:25 +0100

    remove manual backend menu sorting

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,22 +10,17 @@ declare(strict_types=1);
  * @link       http://github.com/erdmannfreunde/contao-portfolio-bundle
  */
 
-array_insert($GLOBALS['BE_MOD']['content'], 2, [
-    'portfolio' => [
-        'tables' => ['tl_portfolio_archive', 'tl_portfolio', 'tl_portfolio_category', 'tl_content'],
-        //'icon'        => 'system/modules/portfolio/assets/icon.png',
-    ],
-]);
+$GLOBALS['BE_MOD']['content']['portfolio'] = [
+    'tables' => ['tl_portfolio_archive', 'tl_portfolio', 'tl_portfolio_category', 'tl_content'],
+];
 
 /*
  * Front end modules
  */
-array_insert($GLOBALS['FE_MOD'], 3, [
-    'portfolio' => [
-        'portfoliolist' => '\\EuF\\PortfolioBundle\\Modules\\ModulePortfolioList',
-        'portfolioreader' => '\\EuF\\PortfolioBundle\\Modules\\ModulePortfolioReader',
-    ],
-]);
+$GLOBALS['FE_MOD']['portfolio'] = [
+    'portfoliolist' => '\\EuF\\PortfolioBundle\\Modules\\ModulePortfolioList',
+    'portfolioreader' => '\\EuF\\PortfolioBundle\\Modules\\ModulePortfolioReader',
+];
 
 $GLOBALS['TL_MODELS']['tl_portfolio'] = '\\EuF\\PortfolioBundle\\Models\\PortfolioModel';
 $GLOBALS['TL_MODELS']['tl_portfolio_archive'] = '\\EuF\\PortfolioBundle\\Models\\PortfolioArchiveModel';
