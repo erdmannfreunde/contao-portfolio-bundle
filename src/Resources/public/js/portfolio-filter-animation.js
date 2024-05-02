98b2c0d56a212f0e0ffb5a2ca4b9b9deece9c16f
Dennis Erdmann <dennis@erdmann-freunde.de>
2024-05-02 10:56:57 +0200

    fix for hiding inactive elements
    use `includes` instead of `contains`,
    cleaning up js

--- src/Resources/public/js/portfolio-filter-animation.js
+++ src/Resources/public/js/portfolio-filter-animation.js
@@ -1,96 +1,92 @@
 function animatePortfolioItems(filters, portfolioItems, duration) {
-    let itemCategory;
-    let animationDuration = duration || 500;
-    let activeFilter;
+  let itemCategory;
+  let animationDuration = duration || 500;
+  let activeFilter;
 
-    filters.forEach((filter) => {
-        
-        if(filter.classList.contains('active')) {
-            activeFilter = filter;
-        }
+  filters.forEach((filter) => {
+    if(filter.classList.contains('active')) {
+      activeFilter = filter;
+    }
 
-        filter.addEventListener('click', (e) => {
-            e.preventDefault();
-            
-            if(activeFilter) {
-                activeFilter.classList.remove('active');
-            }
+    filter.addEventListener('click', (e) => {
+      e.preventDefault();
 
-            e.target.classList.add('active');
-            activeFilter = e.target;
+      if(activeFilter) {
+        activeFilter.classList.remove('active');
+      }
 
-            let filterCategory = activeFilter.dataset.category;
+      e.target.classList.add('active');
+      activeFilter = e.target;
 
-            if (filterCategory == 'all') {
-                portfolioItems.forEach((item) => {
-                    item.classList.remove('portfolio-visible');
+      let filterCategory = activeFilter.dataset.category;
 
-                    fadeIn(item, animationDuration);
-                });
-            } else {
-                for (const item of portfolioItems) {
-                    itemCategory = item.dataset.category;
+      if (filterCategory == 'all') {
+        portfolioItems.forEach((item) => {
+          item.classList.remove('portfolio-visible');
 
-                    if (itemCategory.contains(filterCategory)) {
-                        fadeOut(item, animationDuration).then(function() {
-                            fadeIn(item, animationDuration);
-                        });
-                        
-                    } else {
-                        fadeOut(item, animationDuration);
-                    }
-                }
-            }
+          fadeIn(item, animationDuration);
         });
+      } else {
+        for (const item of portfolioItems) {
+          itemCategory = item.dataset.category;
+
+          if (itemCategory.includes(filterCategory)) {
+            fadeOut(item, animationDuration).then(function() {
+              fadeIn(item, animationDuration);
+            });
+          } else {
+            fadeOut(item, animationDuration);
+          }
+        }
+      }
     });
+  });
 
 
-    function defaultFadeConfig() {
+  function defaultFadeConfig() {
     return {
-        easing: 'linear', 
-        iterations: 1, 
-        direction: 'normal', 
-        fill: 'forwards',
-        delay: 0,
-        endDelay: 0
-        }
+      easing: 'linear',
+      iterations: 1,
+      direction: 'normal',
+      fill: 'forwards',
+      delay: 0,
+      endDelay: 0
     }
+  }
 
-    function fadeOut(el, animationDuration, config = defaultFadeConfig()) {
+  function fadeOut(el, animationDuration, config = defaultFadeConfig()) {
+    return new Promise((resolve) => {
+      const animation = el.animate([
+        { opacity: '1' },
+        { opacity: '0', offset: 0.5 },
+        { opacity: '0', offset: 1 }
+      ], {duration: animationDuration, ...config});
 
-        return new Promise((resolve, reject) => {
-            const animation = el.animate([
-                { opacity: '1' },
-                { opacity: '0', offset: 0.5 },
-                { opacity: '0', offset: 1 }
-            ], {duration: animationDuration, ...config});
-            
-            animation.onfinish = () => {
-                el.style.display = 'none';
-                el.classList.remove('portfolio-visible');
-                el.classList.add('portfolio-hidden');
-                resolve();
-            }
-        })
-    }
+      animation.onfinish = () => {
+        el.style.display = 'none';
+        el.classList.remove('portfolio-visible');
+        el.classList.add('portfolio-hidden');
+        resolve();
+      }
+    })
+  }
 
-    function fadeIn(el, animationDuration, config = defaultFadeConfig()) {
+  function fadeIn(el, animationDuration, config = defaultFadeConfig()) {
+    return new Promise((resolve) => {
+      el.style.display = 'block';
 
-        return new Promise((resolve) => {
-            el.style.display = 'block';
+      const animation = el.animate([
+        { opacity: '0' },
+        { opacity: '0.5', offset: 0.5 },
+        { opacity: '1', offset: 1 }
+      ], {duration: animationDuration, ...config});
 
-            const animation = el.animate([
-                { opacity: '0' },
-                { opacity: '0.5', offset: 0.5 },
-                { opacity: '1', offset: 1 }
-            ], {duration: animationDuration, ...config});
-            
 
-            animation.onfinish = () => {
-                el.classList.add('portfolio-visible');
-                el.classList.remove('portfolio-hidden');
-                resolve();
-            }
-        });
-    }
+      animation.onfinish = () => {
+        el.classList.add('portfolio-visible');
+        el.classList.remove('portfolio-hidden');
+        resolve();
+      }
+    });
+  }
 }
